53534716a7832bbbe2e3a33b0526c6014925fb87
Masato Yamaguchi <m.yamaguchi@creatorsinpack.com>
2015-04-16 16:25:27 +0900

    Add Google API config file

---
+++ src/fuel/app/config/google.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'google' => array(
+        'service_account' => array(
+            'client_id' => '<ServiceAccount ClientID>',
+            'mail_addr' => '<ServiceAccount MailAddr>',
+            'key_file' => 'P12 Key file path'
+        )
+    )
+);
+
+/* End of file google.php */
